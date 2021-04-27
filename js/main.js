89908b2afe4bb994faca48bb4bbc64ac0705ef16
JuHyeong <wngud4680@gmail.com>
2021-04-27 21:54:34 +0900

    feat: Add randomPeep function
    TODO
    - Add randomPeep function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const $scoreCount = document.querySelector('.score-count');
 const $lifeCount = document.querySelector('.life-count');
 const $keyboard = document.querySelector('.keyboard');
 const $keys = document.querySelectorAll('.key');
-
+let lastKey;
 window.addEventListener('keydown', e => {
   const selectedKey = document.querySelector(`.key[data-code="${e.code}"]`);
   const filterKey = [...$keys].filter(key => key === selectedKey);
@@ -18,3 +18,26 @@ const removeTransform = e => {
 $keys.forEach(key => {
   key.addEventListener('transitionend', removeTransform);
 });
+
+// random key
+const randomKey = () => {
+  const randomIndex = Math.floor(Math.random() * $keys.length); // 0 ~ length-1
+  const selectedKey = $keys[randomIndex];
+  if (lastKey === selectedKey) {
+    // 중복되는 키 무시
+    console.log('reduplication!!!!');
+    return randomKey();
+  }
+  lastKey = selectedKey;
+  return selectedKey;
+};
+
+const randomPeep = () => {
+  const selectedKey = randomKey();
+  selectedKey.classList.add('key-sign');
+  setTimeout(() => {
+    selectedKey.classList.remove('key-sign');
+    randomPeep();
+  }, 1300);
+};
+randomPeep();
